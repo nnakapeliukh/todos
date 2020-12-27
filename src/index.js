7e50973433e35c841582f3c4dafe8846ba9f1207
Nazar <nnakapeliukh@gmail.com>
2020-12-26 18:36:06 -0600

    =initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ToDoItem from './todoclass.js';
+// import drawHomeTab from './home_tab.js';
+
+
+
+// export {
+//     drawMainPage as default
+// }
+
+const t = new ToDoItem();
+t.speak();
---
+++ src/todoclass.js
@@ -0,0 +1,8 @@
+class ToDoItem {
+    speak() {
+        alert('to do');
+    }
+    
+}
+
+export {ToDoItem as default};
